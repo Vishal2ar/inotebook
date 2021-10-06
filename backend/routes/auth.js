975a42e781f31a4821039a853c635a9be1f9e2fb
Vishal2ar <88627083+Vishal2ar@users.noreply.github.com>
2021-10-06 19:39:28 +0530

    Validation for User added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,26 @@
 const express =require('express');
 const User = require("../models/Users");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
-router.post("/",(req,res)=> {
-   console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send("hello");
+router.post("/",
+[ body('email',"The email is ot valid").isEmail(),
+body('password').isLength({ min: 5 }),
+body('name').isLength({ min: 5 })]
+,(req,res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+   
+    User.create({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+      }).then(User => res.json(User)).catch(err => {console.log(errors);
+    res.json(errors)})
+  
 })
 
 module.exports = router
